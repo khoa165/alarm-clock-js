475bf343728b064d1e00bc655e1d02fe3d4107b9
Khoa Le <khoatle165@gmail.com>
2019-08-03 23:56:01 +0200

    start alarm

--- app.js
+++ app.js
@@ -1,53 +1,71 @@
-const formatDay = (day) => {
-  switch(day) {
-  case 0:
-    return "Sunday";
-    break;
-  case 1:
-    return "Monday";
-    break;
-  case 2:
-    return "Tuesday";
-    break;
-  case 3:
-    return "Wednesday";
-    break;
-  case 4:
-    return "Thursday";
-    break;
-  case 5:
-    return "Friday";
-    break;
-  case 6:
-    return "Saturday";
-    break;
-  }
+let current = '';
+
+const formatDay = (index) => {
+  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  return days[index];
 }
 
-const formatOneDigit = (number) => {
-  if (number < 10) {
-    return `0${number}`;
-  } else {
-    return number;
-  }
+const formatNumber = (number) => {
+  return number < 10 ? `0${number}` : number;
 };
 
 const displayDateTime = () => {
   const current = new Date();
   const day = formatDay(current.getDay());
-  const date = formatOneDigit(current.getDate());
-  const month = formatOneDigit(current.getMonth() + 1);
+  const date = formatNumber(current.getDate());
+  const month = formatNumber(current.getMonth() + 1);
   const year = current.getFullYear();
   const displayedDate = document.getElementById("date");
   displayedDate.innerText = `${day}, ${month}/${date}/${year}`;
 
-  const hour = current.getHours();
-  const minute = current.getMinutes();
-  const second = formatOneDigit(current.getSeconds());
+  const h = formatNumber(current.getHours());
+  const m = formatNumber(current.getMinutes());
+  const s = formatNumber(current.getSeconds());
   const displayedTime = document.getElementById("time");
-  displayedTime.innerText = `${hour}:${minute}:${second}`;
+  displayedTime.innerText = `${h}:${m}:${s}`;
+  current = displayedTime.innerText;
+};
+
+const addOptions = (field, range) => {
+  const select = document.getElementById(`alarm-${field}`);
+  for (i = 0; i < range; i++) {
+    select.options[i] = new Option(formatNumber(i), i);
+  }
+}
+
+addOptions("hour", 24);
+addOptions("minute", 60);
+addOptions("second", 60);
+
+const getAlarmTime = () => {
+  const hour = document.getElementById('alarm-hour');
+  const minute = document.getElementById('alarm-minute');
+  const second = document.getElementById('alarm-second');
+  const h = hour.options[hour.selectedIndex].value;
+  const m = minute.options[minute.selectedIndex].value;
+  const s = second.options[second.selectedIndex].value;
+  return `${h}:${m}:${s}`;
+};
+
+const startAlarm = () => {
+  const sound = new Audio("https://www.freespecialeffects.co.uk/soundfx/animals/duck1.wav");
+  sound.loop = true;
+
+  document.getElementById('alarm-hour').disabled = true;
+  document.getElementById('alarm-minute').disabled = true;
+  document.getElementById('alarm-second').disabled = true;
+
+  setInterval(() => {
+    if (current === getAlarmTime()) {
+      sound.play();
+    }
+  }, 1000);
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-  setInterval(displayDateTime, 500); // Every 0.5 second.
+  setInterval(displayDateTime, 1000);
 });
+
+document.getElementById("start").addEventListener("click", startAlarm);
+
+document.getElementById("cancel").addEventListener("click", cancelAlarm);
